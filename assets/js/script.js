dd64d7e37ba13fb2cd5283f342836523ddc04f5b
Brian Kaufman <briankaufman@Brians-MBP-5.attlocal.net>
2021-07-23 09:51:33 -0500

    added drag/drop except the lesson taught it wrong so it doesn't actually work. screw this.

--- assets/js/script.js
+++ assets/js/script.js
@@ -180,6 +180,32 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group")
+});
+
+// $(".card .list-group-item").sortable({
+//   connectWith: $(".card .list-group"),
+//   scroll: false,
+//   tolerance: "pointer",
+//   helper: "clone",
+//   activate: function(event) {
+//     console.log("activate", this);
+//   },
+//   deactivate: function(event) {
+//     console.log("deactivate", this);
+//   },
+//   over: function(event) {
+//     console.log("over", event.target);
+//   },
+//   out: function(event) {
+//     console.log("over", event.target);
+//   },
+//   update: function(event) {
+//     console.log("update", this);
+//   }
+// });
+
 // load tasks for the first time
 loadTasks();
 
